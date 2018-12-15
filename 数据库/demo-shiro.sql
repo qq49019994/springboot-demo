a091db7514b99e1da81cb80848b4b81cf6dd9182
王天放 <49019994@qq.com>
2018-12-16 00:25:23 +0800

    数据库
    Signed-off-by: 王天放 <49019994@qq.com>

---
+++ 数据库/demo-shiro.sql
@@ -0,0 +1,85 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 8.0.13 : Database - demo-shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo-shiro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `demo-shiro`;
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(20) NOT NULL COMMENT '权限名称',
+  `permission_url` varchar(50) NOT NULL DEFAULT '#' COMMENT '该权限对应拦截请求链接 #为不拦截',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_PERMISSION_NAME` (`permission_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_permission` */
+
+insert  into `sys_permission`(`id`,`permission_name`,`permission_url`) values (1,'删除用户','#'),(2,'修改用户','#');
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(20) NOT NULL COMMENT '用户组名称',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_ROLE_NAME` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`id`,`role_name`) values (1,'admin');
+
+/*Table structure for table `sys_role_permission` */
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+
+CREATE TABLE `sys_role_permission` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `role_id` int(10) NOT NULL COMMENT '用户组ID',
+  `permission_id` int(10) NOT NULL COMMENT '权限ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_role_permission` */
+
+insert  into `sys_role_permission`(`id`,`role_id`,`permission_id`) values (1,1,1),(2,1,2);
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL COMMENT '账号',
+  `password` char(32) NOT NULL COMMENT '密码',
+  `role_id` int(10) NOT NULL COMMENT '所属用户组',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_USERNAME` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`password`,`role_id`) values (2,'admin','038bdaf98f2037b31f1e75b5b4c9b26e',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ 数据库/demo.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 8.0.13 : Database - demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `demo`;
+
+/*Table structure for table `userinfo` */
+
+DROP TABLE IF EXISTS `userinfo`;
+
+CREATE TABLE `userinfo` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_USERNAME` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `userinfo` */
+
+insert  into `userinfo`(`id`,`username`,`password`) values (1,'admin','123456'),(2,'wangtianfang','123456'),(3,'wangtianfang2','123456');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
